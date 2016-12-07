4e11c80377d02ee34eea87622961b13e2b862e0e
Asdrúbal Suárez <asdrubal.ivan.suarez.rivera@gmail.com>
2016-12-06 20:08:47 -0400

    Adding pages for theme

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-<h1>Hello world</h1>
+<?php 
+    get_header();
+    if(have_posts()):
+        while(have_posts()):
+            the_post();
+?>
+    <h1>
+    <a href="<?php echo the_permalink(); ?>">
+    <?php echo the_title(); ?></a></h1>
+    <article class="post">
+        <?php the_content(); ?>
+    </article>
+<?php
+        endwhile;
+    else:
+        ?>
+    <p>No content found</p>
+
+<?php
+    endif;
+    get_footer();
+?>
